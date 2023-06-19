3c4ee797487d3954e017621dff5fead16c82e670
Devin Carter <devinacarter97@gmail.com>
2023-06-19 15:44:34 +0100

    Added event listeners to operation buttons

--- main.js
+++ main.js
@@ -1,37 +1,212 @@
-const input = document.getElementById('input');
-const output = document.querySelector('p');
-
-output.innerHTML = 'hi';
-
 let num1 = 0;
-let operator = '';
+let currentOperator = '';
 let num2 = 0; 
 
+const container = document.getElementById('calculator-container');
+const internals = document.querySelector('#internals');
+const input = document.querySelector('#input');
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const multiply = document.querySelector('#times');
+const divide = document.querySelector('#divide');
+const equals = document.querySelector('#equals');
 
-function add(a, b) {
+function setDivide() {
+        currentOperator += '/';
+        input.textContent = `\u00f7`;
+        console.log('changed');
+}  
+
+function setAdd() {
+    currentOperator += '*';
+        input.textContent = `\u00d7`;
+        console.log('changed');
+
+}
+
+function setSubtract() {
+    currentOperator += '-';
+    input.textContent = `-`;
+    console.log('changed'); 
+}
+
+function setMultiply() {
+    currentOperator += '*';
+    input.textContent = `\u00d7`;
+    console.log('changed'); 
+}
+
+function doMath() {
+    input.textContent = '=';
+    if (num1, currentOperator, num2) {
+    input.textContent = `${calculatorOp(num1, currentOperator, num2)}`;
+    num1 = 0;
+    currentOperator = '';
+    num2 = 0;
+    }
+
+
+}
+
+function selectZero() {
+    if (!num1) {
+        num1 = 0;
+        input.textContent += '0';
+    }
+    else if (!num2) {
+        num2 = 0;
+        input.textContent += '0';
+    }
+}
+
+function selectOne() {
+    if (!num1) {
+        num1 = 1;
+        input.textContent += '1';
+    }
+    else if (!num2) {
+        num2 = 1;
+        input.textContent += '1';
+    }
+}
+
+function selectTwo() {
+    if (!num1) {
+        num1 = 2;
+        input.textContent += '2';
+    }
+    else if (!num2) {
+        num2 = 2;
+        input.textContent += '2';
+    }
+}
+
+function selectThree() {
+    if (!num1) {
+        num1 = 3;
+        input.textContent += '3';
+    }
+    else if (!num2) {
+        num2 = 3;
+        input.textContent += '3';
+    }
+}
+
+function selectFour() {
+    if (!num1) {
+        num1 = 4;
+        input.textContent += '4';
+    }
+    else if (!num2) {
+        num2 = 4;
+        input.textContent += '4';
+    }
+}
+
+function selectFive() {
+    if (!num1) {
+        num1 = 5;
+        input.textContent += '5';
+    }
+    else if (!num2) {
+        num2 = 5;
+        input.textContent += '5';
+    }
+}
+
+function selectSix() {
+    if (!num1) {
+        num1 = 6;
+        input.textContent += '6';
+    }
+    else if (!num2) {
+        num2 = 6;
+        input.textContent += '6';
+    }
+}
+
+function selectSeven() {
+    if (!num1) {
+        num1 = 7;
+        input.textContent += '7';
+    }
+    else if (!num2) {
+        num2 = 7;
+        input.textContent += '7';
+    }
+}
+
+function selectEight() {
+    if (!num1) {
+        num1 = 8;
+        input.textContent += '8';
+    }
+    else if (!num2) {
+        num2 = 8;
+        input.textContent += '8';
+    }
+}
+
+function selectNine() {
+    if (!num1) {
+        num1 = 9;
+        input.textContent += '9';
+    }
+    else if (!num2) {
+        num2 = 9;
+        input.textContent += '9';
+    }
+}
+
+
+
+divide.addEventListener('click', setDivide);
+add.addEventListener('click', setAdd);
+multiply.addEventListener('click', setMultiply);
+subtract.addEventListener('click', setSubtract);
+equals.addEventListener('click', doMath);
+one.addEventListener('click', selectZero);
+one.addEventListener('click', selectOne);
+one.addEventListener('click', selectTwo);
+one.addEventListener('click', selectThree);
+one.addEventListener('click', selectFour);
+one.addEventListener('click', selectFive);
+one.addEventListener('click', selectSix);
+one.addEventListener('click', selectSeven);
+one.addEventListener('click', selectEight);
+one.addEventListener('click', selectNine);
+
+
+
+function addNums(a, b) {
     return a + b;
 
 }
 
-function subtract(a, b) {
-    return a -b;
+function subtractNums(a, b) {
+    return a - b;
 
 }
 
-function multiply(a, b) {
+function multiplyNums(a, b) {
     return a * b;
 
 }
 
-function divide(a, b) {
+function divideNums(a, b) {
     return a / b;
 
 }
-
-console.log(add(5, 7));
-console.log(subtract(7, 2));
-console.log(multiply(9, 5));
-console.log(divide(10, 2));
 
 function calculatorOp(a, operator, b) {
     if (operator === '+') {
